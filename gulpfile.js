1c2161cdb176cafb5100914f0f260b9e990a3f3f
Andy <w.andrew.keech@gmail.com>
2017-04-23 17:34:48 -0700

    New tasks, relative template paths working

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var cleancss   = require('gulp-clean-css');
 var concat     = require('gulp-concat');
 var dest       = require('gulp-dest');
+var fs         = require('fs');
 var gulp       = require('gulp');
 var gulpPug    = require('gulp-pug');
 var liveServer = require('live-server');
@@ -19,13 +20,19 @@ var liveServerParams = {
     wait: 200,
 };
 
+var files = fs.readdirSync('src/pug/');
+
+gulp.task('fonts', function(){
+    return gulp.src('./src/scss/fonts/**')
+        .pipe(gulp.dest('./build/html/css/assets/fonts/'));
+})
+
 gulp.task('pug', function(){
     return gulp.src('./src/pug/**/*.pug')
         .pipe(gulpPug({
-            filename: ".pug",
             pretty: true,
-            basedir: './'  // This value is required for gulp-pug to resolve the 'include',
-        }))                // 'extends', etc. calls to other files in the build tree.
+            basedir: '',  // This value is required for gulp-pug to resolve the 'include'
+        }))                       // 'extends', etc. calls to other files in the build tree.
         .on('error', swallowError)
         .pipe(gulp.dest('./build/html/'));
 });
@@ -51,10 +58,16 @@ gulp.task('sass', function(){
         .pipe(gulp.dest('./build/html/css/'));
 });
 
-gulp.task('default', ['pug', 'javascript', 'sass'], function () {
+gulp.task('static', function() {
+    return gulp.src('./src/img/**/*')
+        .pipe(gulp.dest('./build/html/img/'));
+});
+
+gulp.task('default', ['fonts', 'pug', 'javascript', 'sass', 'static'], function () {
     gulp.watch('./src/pug/**/*.pug', ['pug']);
     gulp.watch('./src/js/**/*.js', ['javascript']);
     gulp.watch('./src/scss/*.scss', ['sass']);
+    gulp.watch('./src/img/**/*', ['static']);
     liveServer.start(liveServerParams); // As replacement for using the gulp.task('live-server')
 });
 
